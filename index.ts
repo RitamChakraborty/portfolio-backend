689269d0131082178a452646d9f7fe5ac16ffd79
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-02-19 17:57:41 +0530

    Add oak framework

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
 import "https://deno.land/std@0.177.0/dotenv/load.ts";
+import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 
-const PORT = Deno.env.get("PORT") ?? "8080";
+const PORT = +(Deno.env.get("PORT") ?? "8080");
+const app = new Application();
+const router = new Router();
+
+app.use(router.routes());
+
+console.log(`Application is running on https://localhost:${PORT}`);
+await app.listen({ port: PORT });
