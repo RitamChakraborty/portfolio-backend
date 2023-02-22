4a196aff356bfcfa2605b2bf6e682a9f8d7d691c
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-02-22 22:00:55 +0530

    Throw error if gmail username or password is missing

--- src/service/mail-service.ts
+++ src/service/mail-service.ts
@@ -1,10 +1,11 @@
 import {SmtpClient} from "https://deno.land/x/smtp@v0.7.0/mod.ts";
 import {EmailRequest} from "../types.ts";
+import {assert} from "https://deno.land/x/oak@v11.1.0/util.ts";
 
 export async function sendMail(emailRequest: EmailRequest): Promise<boolean> {
     const client = new SmtpClient();
-    const username: string = Deno.env.get("GMAIL_USERNAME")!;
-    const password: string = Deno.env.get("GMAIL_PASSWORD")!;
+    const username: string = Deno.env.get("GMAIL_USERNAME")! ?? assert('Gmail username not found in env');
+    const password: string = Deno.env.get("GMAIL_PASSWORD")! ?? assert('Gmail password not found in env');
 
     console.debug("Username", username);
     console.debug("Password", password);
