c2487eb9aa4d5aa459448809758c9bac84aa2a6f
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-02-22 21:04:20 +0530

    Send mail with the controller

--- src/controllers/mail.ts
+++ src/controllers/mail.ts
@@ -1,11 +1,20 @@
 import {RouteParams, RouterContext} from "https://deno.land/x/oak@v11.1.0/router.ts";
+import {Status} from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import {EmailRequest, ResponseEntity} from "../types.ts";
+import {sendMail} from "../service/mail-service.ts";
 
-export function sendMail(context: RouterContext<"/api/sendMail", RouteParams<string>>) {
+export async function sendMailController(context: RouterContext<"/api/sendMail", RouteParams<string>>) {
+    const requestBody = context.request.body();
+    const emailRequest: EmailRequest = await requestBody.value as EmailRequest;
+    sendMail(emailRequest)
+        .then((result) => {
+            console.info('Email send status:', result);
+        });
     context.response.body = {
-        statusCode: 200,
-        status: "Success",
+        status: Status.OK,
         data: {
-            message: "Mail send successfully",
-        },
-    };
+            message: "Email sent successfully"
+        }
+    } as ResponseEntity;
+    context.response.status = Status.OK;
 }
