23d841f1a431b47c00a8e9ade9072101093fd7a3
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-02-26 14:46:42 +0530

    Add server start log

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
+import "https://deno.land/std@0.177.0/dotenv/load.ts";
 import {Application} from "https://deno.land/x/oak@v11.1.0/mod.ts";
-import router from "./src/routes.ts";
+import emailRouter from "./src/routes/email-router.ts";
+import {info} from "https://deno.land/std@0.177.0/log/mod.ts";
 
 const PORT = +(Deno.env.get("PORT") ?? "8080");
 const app = new Application();
 
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.use(emailRouter.prefix("/api").routes());
+app.use(emailRouter.allowedMethods());
 
 app.addEventListener("listen", ({hostname, port, secure}) => {
-    console.log(`Listening on: ${secure ? "https://" : "http://"}${hostname ?? "localhost"}:${port}`);
+    info(`Listening on: ${secure ? "https://" : "http://"}${hostname ?? "localhost"}:${port}`);
 });
 
 await app.listen({port: PORT});
