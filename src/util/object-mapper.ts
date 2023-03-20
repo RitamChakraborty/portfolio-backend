b237e9385e70ef292458f1c62494d19769c8568b
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-03-20 22:35:15 +0530

    Create object-mapper to convert EmailRequest to SendEmailRequest

---
+++ src/util/object-mapper.ts
@@ -0,0 +1,13 @@
+import {EmailRequest, SendEmailRequest} from "../types.ts";
+
+export default class ObjectMapper {
+    static mapEmailRequestToSendEmailRequest(emailRequest: EmailRequest, receiverEmail: string): SendEmailRequest {
+        return {
+            senderName: emailRequest.name,
+            senderEmail: emailRequest.email,
+            subject: emailRequest.subject,
+            content: emailRequest.content,
+            receiverEmail: receiverEmail
+        };
+    }
+}
