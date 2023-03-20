3d190ea5392f007d0818263b1135d618204338f4
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-03-20 23:51:30 +0530

    Create sendgrid-config

---
+++ src/config/sendgrid-config.ts
@@ -0,0 +1,16 @@
+import {Util} from "../util/util.ts";
+import {Env} from "../data/constant.ts";
+
+type SendgridConfigType = {
+    apiEndpoint: string,
+    authorizationToken: string,
+    masterEmail: string
+}
+
+export default function sendgridConfig(): SendgridConfigType {
+    return {
+        apiEndpoint: Util.getEnv(Env.SENDGRID_API),
+        authorizationToken: Util.getEnv(Env.SENDGRID_AUTH_TOKEN),
+        masterEmail: Util.getEnv(Env.MASTER_EMAIL)
+    };
+}
