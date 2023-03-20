b953b07f63199846d021cc0a14e06a104d972eea
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-03-20 23:53:05 +0530

    Create email-config

---
+++ src/config/email-config.ts
@@ -0,0 +1,14 @@
+import {Util} from "../util/util.ts";
+import {Env} from "../data/constant.ts";
+
+type EmailConfigType = {
+    allowEmail: boolean,
+    masterEmail: string
+}
+
+export default function emailConfig(): EmailConfigType {
+    return {
+        allowEmail: false,
+        masterEmail: Util.getEnv(Env.MASTER_EMAIL)
+    };
+}
