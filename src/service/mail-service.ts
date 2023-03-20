1de1e0d7383b3c9afedde5e84ee3ae9ce5e2dfbb
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-03-20 23:51:06 +0530

    Update mail-service

--- src/service/mail-service.ts
+++ src/service/mail-service.ts
@@ -1,37 +1,36 @@
-import {EmailRequest} from "../types.ts";
-import {Util} from "../util/util.ts";
+import {EmailRequest, SendEmailRequest} from "../types.ts";
 import {EmailType, EndPoint, Env} from "../data/constant.ts";
 import {error, info} from "https://deno.land/std@0.177.0/log/mod.ts";
+import emailConfig from "../config/email-config.ts";
+import sendgridConfig from "../config/sendgrid-config.ts";
+import ObjectMapper from "../util/object-mapper.ts";
+import {Util} from "../util/util.ts";
 
 /**
  * Generates request body to be sent to *Sendgrid* for sending mail
  *
- * @param masterEmail {string} - sendgrid authorized email
- * @param receiverEmail {string} - email of the receiver
- * @param emailRequest {EmailRequest} - sender information
- * @return {string} - string value of the request body
- * @function
+ * @param sendEmailRequest - {@link SendEmailRequest} the object containing mail information
  */
-function createSendgridEmailRequest(masterEmail: string, receiverEmail: string, emailRequest: EmailRequest): string {
+function createSendgridEmailRequest(sendEmailRequest: SendEmailRequest): string {
     const sendgridEmailRequest = {
         personalizations: [
             {
                 to: [
                     {
-                        email: receiverEmail
+                        email: sendEmailRequest.receiverEmail
                     }
                 ]
             }
         ],
         from: {
-            name: emailRequest.name,
-            email: masterEmail
+            name: sendEmailRequest.senderName,
+            email: sendgridConfig().masterEmail
         },
-        subject: emailRequest.subject,
+        subject: sendEmailRequest.subject,
         content: [
             {
                 type: EmailType.TEXT_HTML,
-                value: emailRequest.content
+                value: sendEmailRequest.content
             }
         ]
     }
@@ -40,28 +39,23 @@ function createSendgridEmailRequest(masterEmail: string, receiverEmail: string, 
 }
 
 /**
- * Sends mail through *Sendgrid*
+ * This  method sends email with sendgrid. It takes {@link SendEmailRequest} as an input and based on the data present
+ * in the object, it first creates the header then calls the {@link createSendgridEmailRequest} function to create the
+ * request body to be sent to send grid
  *
- * Get the sendgrid api endpoint, sendgrid authorized email address, receiver email address, sendgrid authorization
- * token from the environment variable. If any of the environment variable is missing, it'll throw an error. It creates
- * the header with the content type and the authorization as bearer token. And send a send mail request to sendgrid.
- * Depending on if the email is sent or not a boolean value is returned.
- *
- * @param emailRequest {EmailRequest} - sender information
- * @return {boolean} - `true` if mail is successfully sent, `false` otherwise
- * @function
+ * @param sendEmailRequest - {@link SendEmailRequest} the object contains mail information
  */
-export async function sendMail(emailRequest: EmailRequest): Promise<boolean> {
-    const sendgridApi: string = Util.getEnv(Env.SENDGRID_API);
-    const sendMailEndpoint = `${sendgridApi}${EndPoint.SENDGRID_SEND_EMAIL}`;
-    const masterEmail: string = Util.getEnv(Env.MASTER_EMAIL);
-    const receiverEmail: string = Util.getEnv(Env.RECEIVER_EMAIL);
-    const authorizationToken: string = Util.getEnv(Env.SENDGRID_AUTH_TOKEN);
+async function sendEmailBySendgrid(sendEmailRequest: SendEmailRequest): Promise<boolean> {
+    if (!emailConfig().allowEmail) {
+        return true;
+    }
+
+    const sendMailEndpoint = `${sendgridConfig().apiEndpoint}${EndPoint.SENDGRID_SEND_EMAIL}`;
     const headers = {
-        "Authorization": `Bearer ${authorizationToken}`,
+        "Authorization": `Bearer ${sendgridConfig().authorizationToken}`,
         "Content-Type": "application/json"
     };
-    const sendgridEmailRequest: string = createSendgridEmailRequest(masterEmail, receiverEmail, emailRequest);
+    const sendgridEmailRequest: string = createSendgridEmailRequest(sendEmailRequest);
     info(`Sendgrid Email Request: ${sendgridEmailRequest}`);
 
     try {
@@ -79,4 +73,23 @@ export async function sendMail(emailRequest: EmailRequest): Promise<boolean> {
     }
 
     return true;
+}
+
+/**
+ * Sends mail through *Sendgrid*
+ *
+ * Get the sendgrid api endpoint, sendgrid authorized email address, receiver email address, sendgrid authorization
+ * token from the environment variable. If any of the environment variable is missing, it'll throw an error. It creates
+ * the header with the content type and the authorization as bearer token. And send a mail request to sendgrid.
+ * Depending on if the email is sent or not a boolean value is returned.
+ *
+ * @param emailRequest {EmailRequest} - sender information
+ * @return {boolean} - `true` if mail is successfully sent, `false` otherwise
+ * @function
+ */
+export function sendMail(emailRequest: EmailRequest): Promise<boolean> {
+    const receiverEmail: string = Util.getEnv(Env.RECEIVER_EMAIL);
+    const sendEmailRequest: SendEmailRequest = ObjectMapper.mapEmailRequestToSendEmailRequest(
+        emailRequest, receiverEmail);
+    return sendEmailBySendgrid(sendEmailRequest);
 }
