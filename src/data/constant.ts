ad61fc6ec5fc061e50d9b54972b71559ebc16125
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-03-20 22:31:07 +0530

    Create project related enum constants

--- src/data/constant.ts
+++ src/data/constant.ts
@@ -2,7 +2,9 @@ export enum Env {
     SENDGRID_API = "SENDGRID_API",
     MASTER_EMAIL = "MASTER_EMAIL",
     RECEIVER_EMAIL = "RECEIVER_EMAIL",
-    SENDGRID_AUTH_TOKEN = "SENDGRID_AUTH_TOKEN"
+    SENDGRID_AUTH_TOKEN = "SENDGRID_AUTH_TOKEN",
+    WINDOW_FRAME_IN_MS = "WINDOW_FRAME_IN_MS",
+    MAX_REQUESTS_ALLOWED = "MAX_REQUESTS_ALLOWED"
 }
 
 export enum EmailType {
@@ -12,4 +14,19 @@ export enum EmailType {
 
 export enum EndPoint {
     SENDGRID_SEND_EMAIL = "/mail/send"
+}
+
+export enum HttpHeader {
+    CONTENT_TYPE = "Content-Type",
+    ACCEPT = "Accept",
+    AUTHORIZATION = "Authorization"
+}
+
+export enum MediaType {
+    APPLICATION_JSON_VALUE = "application/json"
+}
+
+export enum HttpMethod {
+    GET = "GET",
+    POST = "POST"
 }
