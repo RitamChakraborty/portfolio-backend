eb8bc38e5fc9922bbb5cfff77794bc04fa64b05e
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-03-30 02:01:47 +0530

    Make allowed origins dynamic

--- src/config/cors-config.ts
+++ src/config/cors-config.ts
@@ -1,11 +1,10 @@
-import { CorsOptions } from 'https://deno.land/x/cors@v1.2.2/types.ts'
-import { HttpMethod } from '../data/constant.ts'
+import {CorsOptions} from 'https://deno.land/x/cors@v1.2.2/types.ts'
+import {Env, HttpMethod} from '../data/constant.ts'
+import {Util} from '../util/util.ts'
 
 export default function corsConfig(): CorsOptions {
 	return {
-		origin: [
-			'http://localhost:5173',
-		],
+		origin: Util.getEnv(Env.ALLOWED_ORIGINS).split(','),
 		methods: [
 			HttpMethod.POST,
 		],
